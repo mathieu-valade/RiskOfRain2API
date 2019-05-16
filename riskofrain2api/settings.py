ef3d0ac6f207ba08249a74b0fd127ad82dbe7d18
Robin Champsaur <robin.champsaur.dev@gmail.com>
2019-05-16 18:24:34 +0200

    [Settings] Modified settings

--- riskofrain2api/settings.py
+++ riskofrain2api/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATIC_ROOT = '/root/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
