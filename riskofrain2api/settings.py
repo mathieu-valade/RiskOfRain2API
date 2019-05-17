c4cdab72b359c6ce8247d052c471fbbb0f9b727b
MathieuSoSlow <valade.mathieu@gmail.com>
2019-05-17 02:57:08 +0200

    [CI] Add .herokuapp.com to allowed_hosts

--- riskofrain2api/settings.py
+++ riskofrain2api/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY', default="toto")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0', '.herokuapp.com']
 
 
 # Application definition
