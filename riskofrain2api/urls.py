4e0b691e62cda767a7e54d7512164400019a3de0
MathieuSoSlow <valade.mathieu@gmail.com>
2019-06-06 10:56:37 +0200

    [FIX] Remove unused import.

--- riskofrain2api/urls.py
+++ riskofrain2api/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
 from rest_framework import routers
-from rest_framework.documentation import include_docs_urls
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from riskofrain2api.data.views import (
